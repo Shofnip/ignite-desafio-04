23fd0b76e6a648b19cd9ff0a59861ce8504c09cd
Shofnip <shofnip@gmail.com>
2022-12-13 15:38:57 -0300

    Challenge concluded

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,7 +9,15 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const user = this.usersRepository.findById(user_id);
+
+    if (!user || !user.admin) {
+      throw new Error("You do not have permissions!");
+    }
+
+    const users = this.usersRepository.list();
+
+    return users;
   }
 }
 
